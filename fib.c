e560fd7823aec7cd6fa9d1a160681339f279c292
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2024-08-14 18:57:37 +0530

    Add files via upload

---
+++ fib.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ int main()
+ {
+     int a=0;
+     int n,i;
+     int b=1;
+     {
+        if(n==1)
+        return a;
+        if(n==2)
+        return b;
+      for(i=3;i<=n;i++)
+      {
+          
+      }
+    
+     }
+ }
