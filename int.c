27ae48a960d37dc3e6ab9440a8713320f8f46935
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2024-08-14 12:31:01 +0530

    Add files via upload

---
+++ int.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int main()
+  {
+    int P;
+    float A;
+    double B;
+    char C;
+    long  double D;
+    long int E;
+    {
+        printf("Enter the Number: %d",P);
+        scanf("%d \n", &P);
+    }
+     return 0;
+  }
