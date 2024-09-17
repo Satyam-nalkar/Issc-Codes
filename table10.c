1a0f7d708951f1d46d8cab24b1ef4c7b1d48aa0f
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2024-09-17 20:30:09 +0530

    table of 10

---
+++ table10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int n=10;
+    printf("Table of 10 is\n");
+    for(i=1;i<11;i++)
+      {
+        printf("10*%d=%d\n",i,i*n);
+      }
+    return 0;
+}
