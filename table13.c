c857e1ed0704bd1bff95e84bd60583b85ac7f06b
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2024-09-18 17:09:44 +0530

    Table of 13

---
+++ table13.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int n=13;
+    printf("Table of 13 is\n");
+    for(i=1;i<11;i++)
+      {
+        printf("13*%d=%d\n",i,i*n);
+      }
+    return 0;
+}
