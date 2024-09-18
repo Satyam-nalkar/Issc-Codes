fca8eab6ab3828fc4e7db41de0678fd43967cbb5
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2024-09-18 17:08:51 +0530

    Add files via upload

---
+++ table12.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int n=12;
+    printf("Table of 12 is\n");
+    for(i=1;i<11;i++)
+      {
+        printf("12*%d=%d\n",i,i*n);
+      }
+    return 0;
+}
