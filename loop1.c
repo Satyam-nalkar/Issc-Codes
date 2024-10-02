26ca092bdb7424c225bf7f0478efc22b5ee748c9
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2024-10-02 09:59:43 +0530

    Add files via upload

---
+++ loop1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("Enter the positive integer ");
+    scanf("%d",&n);
+    printf("print odd number from 1 to %d\n",n);
+    for ( i = 1; i < n; i++)
+    {
+        if(i % 2 !=0)
+        {
+        printf("%d\n",i);
+        }
+    }
+    return 0;
+}
