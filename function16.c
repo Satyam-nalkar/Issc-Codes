d85d8f0af23280d3a48e62e7c96b915742486f17
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2024-10-07 09:02:06 +0530

    Add files via upload

---
+++ function16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void printSum(int a, int b, int c, int d);
+int main() 
+{
+    int a, b, c,d;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b ,&c);
+    d = a-b-c; 
+    printSum(a, b, c,d);
+    return 0;
+}
+      void printSum(int a, int b, int c, int d) 
+    {
+        printf("The subtractiom is %d:\n",d );
+    }
