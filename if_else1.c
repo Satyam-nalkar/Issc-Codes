6dbd0405d3e93cd2f708585bdb875de4c51b1f29
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2024-10-11 09:54:17 +0530

    Add files via upload

---
+++ if_else1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter an integer");
+    scanf("%d",&n);
+    if(n % 2==0)
+     printf("It is even integer\n");
+     else
+     printf("It is odd integer\n");
+     return 0;
+}
