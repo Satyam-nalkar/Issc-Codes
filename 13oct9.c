aa1e74bd12530cfa2a3cd4dff94f57d2ef87a7f2
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2024-10-18 09:12:24 +0530

    area of circle

---
+++ 13oct9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int radius;
+   
+    printf("Enter the radius");
+    scanf("%d",&radius);
+     double pi=3.14;
+     double Area =pi*radius * radius;
+    printf("Area =  %lf ",Area);
+}
