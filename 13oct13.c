4ad3ff2d153754d6af48577c893ffb44f7129a71
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2024-10-18 09:13:00 +0530

    Add files via upload

---
+++ 13oct13.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int base;
+    int height;
+    printf("Enter the base and height");
+    scanf("%d %d",&base,&height);
+     int c=base * height;
+     float d=0.5;
+    printf("Area =  %f ",c*d);
+}
