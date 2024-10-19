f1eea34935d03bd20a2cd89491e1a949ea514d47
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2024-10-19 10:28:18 +0530

    Add files via upload

---
+++ 13oct14.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two numbers ");
+    scanf("%d %d",&a,&b);
+    if (a > b)
+    
+      printf("a is greater than b\n");
+    
+    else if (a<b)
+    
+      printf("a is less than b\n");
+    
+    else if (a == b)
+    
+       printf("a is equal to b\n");
+    
+      else;
+    return 0;
+    
+}
