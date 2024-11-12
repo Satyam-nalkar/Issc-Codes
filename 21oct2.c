97ae24acdd06160a2aa246f8e9acb22c7e5af988
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2024-11-12 09:09:13 +0530

    Add files via upload

---
+++ 21oct2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,n[5];
+    printf("Enter the integer:");
+    scanf("%d",&n[5]);
+    i=n[5];
+    while (i>=1)
+    {
+       printf("%d\n",i);
+       i--;
+    }
+    return 0;
+}
