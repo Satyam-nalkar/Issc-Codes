4b48c9fe10f62b9ef8caa4b2b5d0aed8e94e5193
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2024-11-14 09:37:25 +0530

    Add files via upload

---
+++ 21oct8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,a[10];
+    printf("Enter 10 integer\n");
+    for ( i = 0; i < 10; i++)
+    {
+      scanf("%d",&a[i]);    
+    }
+    int sum=0;
+    for ( i = 0; i < 10; i++)
+    sum+=a[i];
+    int avg=sum/10;
+    printf("the sum is:%d avg is %d\n",sum,avg);
+    
+  
+}
