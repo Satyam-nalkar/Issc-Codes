1224334c27f318c11d9f4baa3db1aac91b7bd1e8
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2024-11-16 17:46:33 +0530

    Add files via upload

---
+++ 13oct1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{   
+    int i;
+    char name[21];
+    printf("enter your name\n");
+    for(i=0;i<21;i++)
+    {
+    scanf("%c",&name[20]);
+    printf("my name is %s",name);
+    }
+    int age ;
+    printf("enter your age\n");
+    scanf("%d",&age);
+    printf("age =%d",age);
+    char hometown[10];
+    printf("Enter your hometwon\n");
+    {
+    for(i=0;i<10;i++)
+    scanf("%c",&hometown[6]);
+    printf("home =%s",&hometown[6]);
+    }
+    return 0;
+    
+}
+
+
+
+
+
+
