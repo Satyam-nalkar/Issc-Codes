62f1e0e6276042f3615ed5e06ffef74ba26a4f5a
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-02-07 10:58:06 +0530

    Add files via upload

---
+++ c++/minimum_flips_aORb=c.cpp
@@ -0,0 +1,8 @@
+class Solution {
+    public:
+      int minFlips(int a, int b, int c) {
+        return popcount<uint>((a | b) ^ c) + popcount<uint>(a & b & ((a | b) ^ c));
+    }
+        
+        
+    };
