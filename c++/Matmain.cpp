081de28d27f5846b95a71ef0524f48389f734b24
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-02-18 10:03:15 +0530

    Add files via upload

--- c++/Matmain.cpp
+++ c++/Matmain.cpp
@@ -1,72 +1,37 @@
+#include<iostream>
 #include "Matrix.hpp"
+ 
+int main()
+{
+    int rows,cols;
+    cout << "Enter the number of rows and colums:";
+    cin >> rows >> cols;
 
-Matrix::Matrix(int r, int c) : rows(r), cols(c), data(r, std::vector<int>(c, 0)) {}
+   Matrix matrix1(rows,cols);
+   Matrix matrix2(rows,cols);
+   
+   cout << "Enter the value for Matrix 1:\n";
+   matrix1.input();
+  
+   cout << "Enter the value for Matrix 2:\n";
+   matrix2.input();
+    
+   Matrix result = matrix1.add(matrix2);
+   cout << "Resultant sum Matrix:\n";
+   result.display();
+    
+   Matrix result2 = matrix1.sub(matrix2);
+   cout << "Resultant sub Matrix:\n";
+   result2.display();
 
-void Matrix::input() {
-    std::cout << "Enter elements of " << rows << "x" << cols << " matrix:\n";
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            std::cin >> data[i][j];
-        }
+   
+   cout << "\n";
+   if(result.isIdentity()){
+    cout << "the matrix is an identity matrix. \n";
+   }
+   else{
+    cout << "the matrix is not an identity matrix. \n";
     }
-}
+   return 0;
 
-void Matrix::display() const {
-    for (const auto &row : data) {
-        for (int val : row) {
-            std::cout << val << " ";
-        }
-        std::cout << std::endl;
-    }
-}
-
-Matrix Matrix::operator+(const Matrix &other) const {
-    Matrix result(rows, cols);
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            result.data[i][j] = this->data[i][j] + other.data[i][j];
-        }
-    }
-    return result;
-}
-
-Matrix Matrix::operator-(const Matrix &other) const {
-    Matrix result(rows, cols);
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            result.data[i][j] = this->data[i][j] - other.data[i][j];
-        }
-    }
-    return result;
-}
-
-int main() {
-    int rows, cols;
-    std::cout << "Enter number of rows and columns: ";
-    std::cin >> rows >> cols;
-
-    Matrix A(rows, cols);
-    Matrix B(rows, cols);
-
-    std::cout << "Enter values for Matrix A:\n";
-    A.input();
-
-    std::cout << "Enter values for Matrix B:\n";
-    B.input();
-
-    std::cout << "Matrix A:\n";
-    A.display();
-
-    std::cout << "Matrix B:\n";
-    B.display();
-
-    Matrix sum = A + B;
-    std::cout << "Addition Result:\n";
-    sum.display();
-
-    Matrix diff = A - B;
-    std::cout << "Subtraction Result:\n";
-    diff.display();
-
-    return 0;
 }
