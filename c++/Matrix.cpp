22383f23bccb3e914411715e344eae715b0e4a70
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-02-18 10:03:36 +0530

    Add files via upload

--- c++/Matrix.cpp
+++ c++/Matrix.cpp
@@ -1,9 +1,12 @@
 #include "Matrix.hpp"
-using namespace std;
-Matrix::Matrix(int r, int c) : rows(r), cols(c), data(r, vector<int>(c, 0)) {}
+
+Matrix::Matrix(int r, int c) {
+    rows = r;
+    cols = c;
+}
 
 void Matrix::input() {
-    cout << "Enter elements of " << rows << "x" << cols << " matrix:\n";
+    cout << "Enter elements for " << rows << "x" << cols << " matrix:\n";
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             cin >> data[i][j];
@@ -12,31 +15,45 @@ void Matrix::input() {
 }
 
 void Matrix::display() const {
-    for (const auto &row : data) {
-        for (int val : row) {
-            cout << val << " ";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << data[i][j] << " ";
         }
-            cout << endl;
+        cout << endl;
     }
 }
 
-Matrix Matrix::operator+(const Matrix &other) const {
+Matrix Matrix::add(const Matrix& second) const {
     Matrix result(rows, cols);
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            result.data[i][j] = this->data[i][j] + other.data[i][j];
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result.data[i][j] = data[i][j] + second.data[i][j];
         }
     }
     return result;
 }
 
-Matrix Matrix::operator-(const Matrix &other) const {
-    Matrix result(rows, cols);
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            result.data[i][j] = this->data[i][j] - other.data[i][j];
-        }
+Matrix Matrix::sub(const Matrix & second) const {
+ Matrix result2(rows, cols);
+ for(int i=0; i<rows; i++){
+    for(int j=0; j<cols; j++){
+   result2.data[i][j] = data[i][j] - second.data[i][j];
     }
-    return result;
+ } 
+   return result2;
 }
 
+  bool Matrix::isIdentity() const 
+ {
+   if(rows != cols)
+   return false;
+
+   for(int i =0; i<rows; i++){
+    for(int j=0; j<cols; j++){
+        if(i == j && data[i][j] !=1  ||  i != j && data[i][j] !=0){
+        return false;      
+        }
+    }
+   }
+   return true;
+ }
