c64535b023d5e7992f9d4ea07dd3a233c2c10f54
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-02-26 12:59:45 +0530

    Add files via upload

---
+++ c++/First_unique_character_in_string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+        int firstUniqChar(string s) {
+            unordered_map<char,int>freq;
+            queue<int>Q;
+            for(int i=0; i<s.size(); i++)
+         {
+            freq[s[i]]++;
+            Q.push(i);
+            while(!Q.empty() && freq[s[Q.front()]] > 1)
+            Q.pop();
+         }
+         return Q.empty() ? -1 : Q.front();
+        }
+    };
