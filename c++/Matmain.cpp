5e4667e20a76d5dcec082148e42ffe4a2dd6baf7
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-03-02 10:42:29 +0530

    Add files via upload

--- c++/Matmain.cpp
+++ c++/Matmain.cpp
@@ -17,21 +17,47 @@ int main()
    matrix2.input();
     
    Matrix result = matrix1.add(matrix2);
-   cout << "Resultant sum Matrix:\n";
+   cout << "Summation of Matrix:\n";
    result.display();
     
    Matrix result2 = matrix1.sub(matrix2);
-   cout << "Resultant sub Matrix:\n";
+   cout << "Subtraction of Matrix:\n";
    result2.display();
+   
+   Matrix result3 = matrix1.mult(matrix2);
+   cout << "Multiplication of Matrix:\n";
+   result3.display();
 
    
    cout << "\n";
-   if(result.isIdentity()){
+   if(result3.isIdentity()){
     cout << "the matrix is an identity matrix. \n";
    }
    else{
     cout << "the matrix is not an identity matrix. \n";
     }
-   return 0;
+   
 
+
+   cout<<"\n";
+   if(result.isSymmetric()){
+    cout << "the matrix is a symmetric matrix. \n";
+   }
+   else{
+    cout << "the matrix is not a symmetric matrix. \n";
+    }
+
+  cout<<"Gauss elimination with basic pivoting.\n";
+  matrix1.gaussElimination();
+
+  double x[rows];
+  cout << "Back sunstitution.\n";
+  matrix1.backSubstitution(x);
+
+  cout<<"values for x \n";
+  for(int i=0;i<rows;i++){
+    cout << "x[" << i << "] = " << x[i] << endl;
+  }
+
+   return 0;
 }
