93d7f6a73b6209a1e31c7fbf9f227ee7e2790e85
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-05-28 21:43:15 +0530

    program

---
+++ code28.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+// int main(){
+   
+//     int a= 5 - 2* 6;
+//    cout << a <<endl;
+int x= 10;
+void fun(){
+    for (int i = 1; i < 11; i++)
+    {
+      cout << x*i << endl;              
+    }
+    
+}
+
+int main (){
+    fun();
+    // cout << x << endl;
+    return 0;
+}
+
+ 
