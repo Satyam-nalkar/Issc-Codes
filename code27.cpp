77a7978819cf3a12c8d579429da076fa8630192f
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-06-01 20:17:53 +0530

    code 27

---
+++ code27.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int a = 4, b =8;
+   
+   // cout << (a & b) <<endl;
+   // cout << (a | b) <<endl;
+   // cout << (a ^ b) <<endl;    //(same = 0 , diff = 1)
+   // cout << (4 << 1) <<endl;
+   cout << (10 << 2) <<endl;
+   cout << (10 >> 1) <<endl;
+   
+   return 0;
+}
