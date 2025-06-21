005b13083508478a431b964b6549ec059e3be83f
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-06-21 09:26:25 +0530

    code number 7

---
+++ code7.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+   int count = 1;
+   while(count <=20){
+   cout << count << " ";
+   count ++;
+   }   
+   cout << endl;
+    return 0;
+}
