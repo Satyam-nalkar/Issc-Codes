ccc145c2bf762f14b9e2009beb1afb8aef7fe7a1
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-06-22 09:55:38 +0530

    code number 6

---
+++ code6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //ternary statement
+    int n ;
+    cout << "Enter a number\n";
+    cin >> n;
+    cout << (n >=0 ? "positive" : "negative")<<endl;
+    return 0;
+}
