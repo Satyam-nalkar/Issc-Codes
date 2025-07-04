cd4f6bee64cb00ceef172561d0b7318a9ff36e1b
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-07-04 14:14:06 +0530

    string split into character

---
+++ string2.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+//    string str = "hello world";
+//    cout << str << endl;
+
+//    str = "hello";
+//    cout << str << endl;
+
+
+
+
+
+
+//  string str1 = "good";
+//  string str2 = "good";
+
+//  cout << (str1 == str2) << endl; 
+  
+// string str;
+// getline(cin,str);
+
+// cout << "output :" << str << endl;
+
+
+
+
+string str = "apna college";
+ for (int i = 0; i < str.length(); i++)
+ {
+cout << str[i] << " ";
+ }
+ cout << endl;
+   
+return 0;
+
+}
