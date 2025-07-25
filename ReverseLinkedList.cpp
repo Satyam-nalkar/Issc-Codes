d6d6bf3a0141947a30f2c89855d78dc827890506
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-07-25 11:25:57 +0530

    reverse the linked list

--- ReverseLinkedList.cpp
+++ ReverseLinkedList.cpp
@@ -1,2 +1,16 @@
-#include<iostream>
-using namespace std;
+class Solution {
+    public:
+    ListNode* reveseList(ListNode head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
+
+        while(curr != NULL){
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr -> next
+        }
+        return prev;
+    }
+}
