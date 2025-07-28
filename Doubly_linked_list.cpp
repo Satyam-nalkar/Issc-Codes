220539e95e9bae9b0095fb30aab7d90c607a9a1f
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-07-28 18:54:58 +0530

    doubly linked list

--- Doubly_linked_list.cpp
+++ Doubly_linked_list.cpp
@@ -41,16 +41,64 @@
             temp = temp -> next;
         }
         cout << "NULL\n";
-     }
+    
+    }
+
+    void push_back(int val){
+        Node* newNode = new Node(val);
+
+        if(head == NULL){
+            head = tail = newNode;
+        }else{
+        newNode -> prev = tail;
+        tail -> next = newNode;
+        tail = newNode;
+        } 
+    }
+
+
+void pop_front(){
+   if(head == NULL){
+     cout << "DLL is empty\n";
+   }
+
+    Node*temp = head;
+    head = head -> next;
+
+    if(head != NULL){
+        head -> prev =NULL;
+    }
+     temp -> next = NULL;
+     delete temp;
+}
+  
+  void pop_back(){
+    if(head == NULL){
+        cout << "DLL is empty\n";
+        return;
+    }
+    Node * temp = tail;
+    tail = tail -> prev;
+
+    if(tail != NULL){
+        tail -> next = NULL;
+    }
+    temp -> prev = NULL;
+    delete temp;
+  }
+
 };
 
 
  int main(){
   DoublyList dll;
 
-  dll.push_front(1);
-  dll.push_front(2);
-  dll.push_front(3);
+  dll.push_back(1);
+  dll.push_back(2);
+  dll.push_back(3);
+
+  dll.pop_back();
+  dll.pop_back();
 
   dll.print();
     return 0;
