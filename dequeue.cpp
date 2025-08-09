9ac96aa195bcc02e230b63ab3107ee5b1f8e8870
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-08-09 20:35:58 +0530

    dequeue operations

--- dequeue.cpp
+++ dequeue.cpp
@@ -7,8 +7,12 @@ int main(){
     dq.push_back(1);     
     dq.push_back(2);     
     dq.push_back(3);
+    dq.push_back(4);
+    dq.push_back(5);
+    dq.push_back(6);
 
-    dq.pop_back(); // 4, 1, 2, 3
+    dq.pop_back(); 
     cout<< dq.front() <<" " << dq.back() << endl; 
     return 0;
 }
+ 
