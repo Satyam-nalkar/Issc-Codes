cca266bb907b00c0846eb50ca88fc2456ba33540
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-08-26 10:43:05 +0530

    square pattern

--- square.cpp
+++ square.cpp
@@ -4,7 +4,7 @@ int main(){
     int n = 4;
     for(int i = 1;i<=n;i++){
         for(int j = 1; j<=n;j++){
-            cout <<" "<< j;
+            cout << j;
         }
         cout << endl;
     }
