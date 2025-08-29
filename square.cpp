c1e042917f2122ee3b374ed8ee6156299ebe65c0
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-08-29 09:48:34 +0530

    square pattern program

--- square.cpp
+++ square.cpp
@@ -13,7 +13,7 @@
 
 
 
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 int main(){
     int n = 4;
@@ -27,20 +27,20 @@ int main(){
   }
     
     return 0;
-}   
+}   */
  
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int n = 3;
-//     int num = 1;
-//     for(int i = 0;i<n;i++){
-//         for(int j = 0;j<n; j++){
-//             cout <<num << " ";
-//             num ++;
-//         }    
-//         cout <<endl;
-//     }
-//     cout << "after pattern : " << num << endl;
-//     return 0;
-// }
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 3;
+    int num = 1;
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n; j++){
+            cout <<num << " ";
+            num ++;
+        }    
+        cout <<endl;
+    }
+    cout << "after pattern : " << num << endl;
+    return 0;
+}
