48ca112d1e6447c1520b3d1ce749f199ce734f09
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-08-30 09:46:37 +0530

    triangle shape

--- inverted_triangle.cpp
+++ inverted_triangle.cpp
@@ -1,20 +1,48 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n = 4;
+//   for(int i = 0; i<n; i++){
+//     //for space
+//     for(int j = 0; j<i; j++){
+//        cout << " ";
+//     }
+//     //for numbers
+//     for(int j = 0; j<n-i; j++){
+//           cout << i+1; 
+//     }
+//     cout << endl;
+//   }
+
+//     return 0; 
+// } 
+
+
+
+
 #include<iostream>
 using namespace std;
 int main(){
-    int n = 4;
+    int n = 8;
   for(int i = 0; i<n; i++){
-    //for space
-    for(int j = 0; j<i; j++){
+      //for space
+      for(int j = 1; j <= n-i-1; j++){
        cout << " ";
-    }
-    //for numbers
-
-    for(int j = 0; j<n-i; j++){
-          cout << i+1; 
-    }
+       }
+      //for numbers  
+      for(int j = 1; j <= i+1; j++){
+          cout << j; 
+       }
+       //for numbers
+      for(int j = i; j >= 1; j--){
+          cout << j; 
+       }
     cout << endl;
   }
 
     return 0; 
 } 
+
+
+
 
