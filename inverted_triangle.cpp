d5943b937a64404ae5ef88b83a3a0586dd812cbf
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-08-31 10:42:24 +0530

    inverted triangle shape

--- inverted_triangle.cpp
+++ inverted_triangle.cpp
@@ -20,29 +20,72 @@
 
 
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n = 8;
+//   for(int i = 0; i<n; i++){
+//       //for space
+//       for(int j = 1; j <= n-i-1; j++){
+//        cout << " ";
+//        }
+//       //for numbers  
+//       for(int j = 1; j <= i+1; j++){
+//           cout << j; 
+//        }
+//        //for numbers
+//       for(int j = i; j >= 1; j--){
+//           cout << j; 
+//        }
+//     cout << endl;
+//   }
+
+//     return 0; 
+// } 
+
+
+
+
+//HoloDiamond
+
 #include<iostream>
 using namespace std;
 int main(){
-    int n = 8;
+    int n = 4;
+    //top 
   for(int i = 0; i<n; i++){
-      //for space
-      for(int j = 1; j <= n-i-1; j++){
-       cout << " ";
-       }
-      //for numbers  
-      for(int j = 1; j <= i+1; j++){
-          cout << j; 
-       }
-       //for numbers
-      for(int j = i; j >= 1; j--){
-          cout << j; 
-       }
+      for(int j = 0; j<n-i-1;j++){
+        cout << " ";
+      }
+
+    cout << "*";
+
+    if(i != 0){
+      //space
+      for(int j = 0; j<2*i-1;j++){
+        cout << " ";
+      }  
+      cout << "*";
+    }
     cout << endl;
   }
 
+
+    // bottom part
+     for(int i = 0; i < n-1; i++){
+    //space
+    for(int j = 0; j < i+1;j++){
+      cout << " ";
+      }
+     cout << "*";
+     if(i != n-2){
+      //space
+     for(int j = 0; j<2*(n-i)-5; j++){
+        cout << " ";
+      }
+      cout << "*";
+    }
+    cout << endl;
+  }
     return 0; 
-} 
-
-
-
-
+}
