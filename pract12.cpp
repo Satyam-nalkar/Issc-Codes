62fcb67dbfbcd2a5c2476a6a7767dc0655f13b20
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-09-14 17:41:00 +0530

    table of 16

--- pract12.cpp
+++ pract12.cpp
@@ -5,8 +5,9 @@ int main(){
     int n = 16;
     cout <<"Table of a number"<< endl;
     for(int i = 1; i <= 10; i++){
-        int a = i * n;
-        cout << a << endl;
+        
+        int A = i * n;
+        cout << A << endl;
     }
     return 0;
 }
