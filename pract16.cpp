771e31b545e3e54bce80f4ab4323ac87c2b75a67
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-09-18 09:28:37 +0530

    Table of 20

---
+++ pract16.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = 20;
+    cout <<"Table of a number"<< endl;
+    for(int i = 1; i <= 10; i++){
+        
+        int A = i * n;
+        cout << A << endl;
+    }
+    return 0;
+}
